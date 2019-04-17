5395b870925082239d69e2f32a25f00b9f97cb30
dell1985 <cn20060101@outlook.com>
2019-04-18 01:18:11 +0800

    数据清理

--- mydata1.py
+++ mydata1.py
@@ -1,3 +1,19 @@
+
+
 import pandas as pd
 import numpy as np
 import csv
+import matplotlib.pyplot as plta
+import time
+import sqlite3 as sql
+
+
+df=pd.read_csv('pm2.csv')
+print("打开了数据库")
+n0=np.arange(5)
+print(n0)
+n=pd.Series(np.arange(10),index=np.arange(1,20,2))
+print(n)
+print(df.columns)
+df=df.reindex(columns=['合约代码','期货公司名称','持买单','增减量'])
+print(df.合约代码)
