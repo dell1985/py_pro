a735099619c8d1a9803bfe480c33790a6b8be629
dell1985 <cn20060101@outlook.com>
2019-04-21 16:34:28 +0800

    修改了数据文件的存放位置，交易日历放在data0,交易数据放在data

--- read_data.py
+++ read_data.py
@@ -8,12 +8,7 @@ import time
 import sqlite3 as sql
 
 
-df=pd.read_csv('pm2.csv')
-print("打开了数据库")
-n0=np.arange(5)
-print(n0)
-n=pd.Series(np.arange(10),index=np.arange(1,20,2))
-print(n)
+df=pd.read_csv('data/pm2.csv')
 print(df.columns)
 df=df.reindex(columns=['合约代码','期货公司名称','持买单','增减量'])
 print(df.合约代码)
